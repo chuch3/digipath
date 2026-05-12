82785bf5c794de583c15861e89d47e6423fa316f
chuch3 <polychch3@gmail.com>
2026-05-12 20:20:47 +0800

    docs: little edits in text

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 # from openslide import open_slide
 
+import matplotlib.pyplot as plt
 from pydicom import dcmread
 
 from config import _SAMPLE_TCGA_DCM_FILE
 
 
-def main():
+def tissue_segment():
     pass
-    slide = dcmread(_SAMPLE_TCGA_DCM_FILE.as_posix())
-    print()
+
+
+def main():
+    ds = dcmread(_SAMPLE_TCGA_DCM_FILE)
+    plt.imshow(ds.pixel_array)
+
+    plt.show()
 
 
 if __name__ == "__main__":
